e2958b06ae857cb376d50e15796831363415bc00
João Souza <joaovitorpsouza11@gmail.com>
2023-01-19 15:34:32 -0300

    Aula 11

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40.5
+if (vel > 60) {
+  console.log('Você ultrapassou a velocidade permitida. MULTADO')
+}
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+console.log('Dirija sempre utilizando cinto de segurança!')
