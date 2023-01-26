de8e5588cfd91b9bc53d788f22ab22146a8f62b8
João Souza <joaovitorpsouza11@gmail.com>
2023-01-26 16:59:51 -0300

    Update Ex001

--- Exercicios/Ex001/script.js
+++ Exercicios/Ex001/script.js
@@ -7,15 +7,15 @@ function carregar() {
   
   msg.innerHTML = `Agora são ${hora}:${minutos}h.`
   if (hora >= 0 && hora < 12) {
-    img.src = 'Imagens/manha.jpg'
+    img.src = 'images/manha.jpg'
     document.body.style.background = '#BAD0D9'
     msg.innerHTML += '<p>Bom dia!</p>'
   } else if (hora >= 12 && hora < 18) {
-    img.src = 'Imagens/tarde.jpg'
+    img.src = 'images/tarde.jpg'
     document.body.style.background = '#4A7A8C'
     msg.innerHTML += '<p>Boa tarde!</p>'
   } else {
-    img.src = 'Imagens/noite.jpg'
+    img.src = 'images/noite.jpg'
     document.body.style.background = '#0367A6'
     msg.innerHTML += '<p>Boa noite!</p>'
   }
