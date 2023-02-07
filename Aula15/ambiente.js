d9d03ea5c2643dcf0479dc0040a01e2e0d8766d6
João Souza <joaovitorpsouza11@gmail.com>
2023-02-07 14:04:06 -0300

    Updated Aula15

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,16 +1,16 @@
 let num = [5, 8, 2, 9, 4]
 
-console.log(`Nosso vetor é o ${num}`)
+console.log(`Our vector it's ${num}`)
 
 num[5] = 6
 num.push(7)
-console.log(`Nosso vetor agora é o ${num}`)
+console.log(`Now our vector it's ${num}`)
 
 num.sort()
-console.log(`Na ordem crescente fica ${num}`)
+console.log(`In ascending order it's ${num}`)
 
 for (c in num) {
   console.log(num[c])
 }
 
-console.log(`O 8 está na posição ${num.indexOf(8)}`)
+console.log(`The number 8 it's on position ${num.indexOf(8)}`)
